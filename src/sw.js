88b053516d1d0de304d1d56ea64564a82de3d536
marmooo <marmooo@users.noreply.github.com>
2021-06-24 07:12:15 +0900

    bump bootstrap from 5.0.1 to 5.0.2

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-06-09 09:50';
+var CACHE_NAME = '2021-06-24 07:06';
 var urlsToCache = [
   '/type-100masu/',
   '/type-100masu/index.js',
@@ -6,7 +6,7 @@ var urlsToCache = [
   '/tegaki-100masu/mp3/end.mp3',
   '/tegaki-100masu/mp3/correct3.mp3',
   'https://marmooo.github.io/fonts/textar-light.woff2',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
 ];
 
 self.addEventListener('install', function(event) {
