7359e5f24c2d53b37d1a52b76f2e9259e9771285
marmooo <marmooo@users.noreply.github.com>
2025-12-04 02:44:38 +0900

    improve service worker

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,5 @@
-const CACHE_NAME = "2025-11-13 00:00";
+const cacheName = "2025-12-04 00:00";
 const urlsToCache = [
-  "/type-100masu/",
   "/type-100masu/index.js",
   "/type-100masu/mp3/end.mp3",
   "/type-100masu/mp3/correct3.mp3",
@@ -8,29 +7,35 @@ const urlsToCache = [
   "https://marmooo.github.io/fonts/textar-light.woff2",
 ];
 
+async function preCache() {
+  const cache = await caches.open(cacheName);
+  await Promise.all(
+    urlsToCache.map((url) =>
+      cache.add(url).catch((e) => console.warn("Failed to cache", url, e))
+    ),
+  );
+  self.skipWaiting();
+}
+
+async function handleFetch(event) {
+  const cached = await caches.match(event.request);
+  return cached || fetch(event.request);
+}
+
+async function cleanOldCaches() {
+  const cacheNames = await caches.keys();
+  await Promise.all(
+    cacheNames.map((name) => name !== cacheName ? caches.delete(name) : null),
+  );
+  self.clients.claim();
+}
+
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    }),
-  );
+  event.waitUntil(preCache());
 });
-
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    }),
-  );
+  event.respondWith(handleFetch(event));
 });
-
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
-          .map((cacheName) => caches.delete(cacheName)),
-      );
-    }),
-  );
+  event.waitUntil(cleanOldCaches());
 });
